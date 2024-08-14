b82d74e74e67fe4d48f78af2fa8915a8388f4573
Mohsin0306 <144019590+Mohsin0306@users.noreply.github.com>
2024-08-14 09:49:08 -0700

    Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,31 +1,52 @@
-import React, { useState } from 'react';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../Styles/Header.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faHeart, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
-  const [navOpen, setNavOpen] = useState(false);
-
-  const toggleNav = () => {
-    setNavOpen(prevState => !prevState);
-  };
-
   return (
-    <>
-      <header className="header">
-        <div className="logo">E-shop</div>
-        <div className="hamburger" onClick={toggleNav}>
-          <FontAwesomeIcon icon={faBars} />
-        </div>
-        <nav className={`nav ${navOpen ? 'open' : ''}`}>
-          <a href="/">Home</a>
-          <a href="/products">Products</a>
-          <a href="/service">Service</a>
-          <a href="/contact">Contact</a>
+    <header className="header">
+      <div className="container">
+        <nav className="navbar navbar-expand-lg">
+          <a className="navbar-brand" href="#">
+            <h1>E-Shop</h1>
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav mx-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="#home">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#products">Products</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#service">Service</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#contact">Contact</a>
+              </li>
+            </ul>
+            <div className="navbar-icons">
+              <FontAwesomeIcon icon={faSearch} className="mr-3" />
+              <FontAwesomeIcon icon={faHeart} className="mr-3" />
+              <FontAwesomeIcon icon={faShoppingCart} />
+            </div>
+          </div>
         </nav>
-        <div className={`overlay ${navOpen ? 'show' : ''}`} onClick={toggleNav}></div>
-      </header>
-    </>
+      </div>
+    </header>
   );
 };
 
